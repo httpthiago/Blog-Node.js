70c7893953dabf5246a1e38cacdb4a2b7032fc77
teagokkk <thiagogamerhd@gmail.com>
2021-11-19 12:12:38 -0300

    adicionando esqueleto projeto

---
+++ app.js
@@ -0,0 +1,25 @@
+// Módulos
+  const express = require('express');
+  const handlebars = require('express-handlebars');
+  const mongoose = require('mongoose');
+  const app = express();
+
+// Configurações
+  // Formato de entrada
+    app.use(express.urlencoded({extended: false}));
+    app.use(express.json());
+
+  // Template Engine (Handlebars)
+    const hbs = handlebars.create({defaultLayout: 'main'});
+    app.engine('handlebars', hbs.engine);
+    app.set('view engine', 'handlebars');
+  
+  // Mongoose
+    // TODO
+
+// Rotas
+// Outros
+  const PORT = 8081;
+  app.listen(PORT, () => {
+    console.log('Servidor funcionando com sucesso!');
+  })
