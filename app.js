840e2b9d098b23e3ec64436f9dd2d616fdad2398
teagokkk <thiagogamerhd@gmail.com>
2021-11-22 10:25:15 -0300

    configurando session e middleware

--- app.js
+++ app.js
@@ -4,9 +4,26 @@
   const mongoose = require('mongoose');
   const adminRoute = require('./routes/admin');
   const path = require('path');
+  const session = require('express-session');
+  const flash = require('connect-flash');
   const app = express();
 
 // Configurações
+  // Session
+    app.use(session({
+      secret: 'cursodenode',
+      resave: true,
+      saveUninitialized: true
+    }))
+
+    app.use(flash());
+
+  // Middleware
+    app.use((req, res, next) => {
+      res.locals.success_msg = req.flash('success_msg');
+      res.locals.error_msg = req.flash('error_msg');
+      next();
+    })
   // Formato de entrada
     app.use(express.urlencoded({extended: false}));
     app.use(express.json());
