d59531386213826ecca983b963ad2808b76d61c1
teagokkk <thiagogamerhd@gmail.com>
2021-11-23 13:16:06 -0300

    adicinado listagem, edicao e remocao de categorias

--- routes/admin.js
+++ routes/admin.js
@@ -9,7 +9,11 @@ router.get('/', (req, res) => {
 })
 
 router.get('/categorias', (req, res) => {
-  res.render('admin/categorias');
+  Categoria.find().lean().then((categorias) => {
+    res.render('admin/categorias', { categorias: categorias })
+  }).catch((err) => {
+    res.send('Erro ao listar categorias: ' + err)
+  })
 })
 
 router.get('/categorias/add', (req, res) => {
@@ -52,6 +56,42 @@ router.post('/categorias/nova', (req, res) => {
 
 
 
+})
+
+//editar
+router.get('/categorias/editar/:id', (req, res) => {
+  Categoria.findOne({ _id: req.params.id }).lean().then((categorias) => {
+    res.render('admin/editcategoria', { categorias: categorias })
+  })
+})
+
+router.post('/categorias/editar', (req, res) => {
+  Categoria.findOne({ _id: req.body.id }).then((categoria) => {
+    categoria.nome = req.body.nome
+    categoria.slug = req.body.slug
+
+    categoria.save().then(() => {
+      req.flash('success_msg', 'Categoria editada com sucesso!')
+      res.redirect('/admin/categorias')
+    }).catch((err) => {
+      req.flash('error_msg', 'Erro ao editar: ' + err)
+    })
+
+
+  })
+})
+
+
+
+//excluir
+router.post('/categorias/excluir', (req, res) => {
+  Categoria.deleteOne({ _id: req.body.id }).then(() => {
+    req.flash('success_msg', 'Categoria excluída com sucesso!')
+    res.redirect('/admin/categorias')
+  }).catch((err) => {
+    req.flash('error_msg', 'Erro ao excluir: ' + err)
+    res.redirect('/admin/categorias')
+  })
 })
 
 router.get('/postagens', (req, res) => {
